ea43de1846aca469216e89e0d33cb611f18c418f
kgibs <kelley@thephuse.com>
2014-03-17 11:57:45 -0400

    Remove extra code that was left in by accident.

--- winter/scripts/main.js
+++ winter/scripts/main.js
@@ -123,35 +123,3 @@ $(function () {
   });
 
 });
-
-
-
-var m_names = new Array("January", "February", "March", 
-"April", "May", "June", "July", "August", "September", 
-"October", "November", "December");
-
-var d = new Date();
-var curr_date = d.getDate();
-var sup = "";
-if (curr_date == 1 || curr_date == 21 || curr_date ==31)
-   {
-   sup = "st";
-   }
-else if (curr_date == 2 || curr_date == 22)
-   {
-   sup = "nd";
-   }
-else if (curr_date == 3 || curr_date == 23)
-   {
-   sup = "rd";
-   }
-else
-   {
-   sup = "th";
-   }
-
-var curr_month = d.getMonth();
-var curr_year = d.getFullYear();
-
-document.write(curr_date + "<SUP>" + sup + "</SUP> " 
-+ m_names[curr_month] + " " + curr_year);
