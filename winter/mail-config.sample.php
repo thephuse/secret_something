7900d9f07ff6e2340cd91d304ff99851cfd0019b
Kelley Gibson <kelley@thephuse.com>
2014-11-30 21:50:53 -0500

    Add config sample for others' use

---
+++ winter/mail-config.sample.php
@@ -0,0 +1,9 @@
+<?php 
+  $mail_config_isSMTP = true;                     // Set mailer to use SMTP
+  $mail_config_Host = 'smtp1.example.com';        // Specify main and backup SMTP servers
+  $mail_config_SMTPAuth = true;                   // Enable SMTP authentication
+  $mail_config_Username = 'user@example.com';     // SMTP username
+  $mail_config_Password = 'secret';               // SMTP password
+  $mail_config_SMTPSecure = 'tls';                // Enable TLS encryption, `ssl` also accepted
+  $mail_config_Port = 587;                        // TCP port to connect to
+?>
