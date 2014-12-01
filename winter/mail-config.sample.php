0b2078e363972249f3daa1383a9b714ff69777bb
Kelley Gibson <kelley@thephuse.com>
2014-12-01 11:21:29 -0500

    Fix mobile overflow issue on participants list.

--- winter/mail-config.sample.php
+++ winter/mail-config.sample.php
@@ -1,6 +1,6 @@
 <?php 
   $mail_config_isSMTP = true;                     // Set mailer to use SMTP
-  $mail_config_Host = 'smtp1.example.com';        // Specify main and backup SMTP servers
+  $mail_config_Host = 'smtp1.example.com';        // Specify main and backup SMTP servers, for Mandrill use smtp.mandrillapp.com
   $mail_config_SMTPAuth = true;                   // Enable SMTP authentication
   $mail_config_Username = 'user@example.com';     // SMTP username
   $mail_config_Password = 'secret';               // SMTP password
